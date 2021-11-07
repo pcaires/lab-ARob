0998cb0004a7debf0162d8f0350e4d667cc72814
Pedro <Pedrosilva704@hotmail.com>
2021-11-07 20:16:56 +0000

    Parse 4 e parse 43
    algumas alterações a ficheiros de dados, nada que interfira com as outras secções

---
+++ parse43.m
@@ -0,0 +1,16 @@
+load 43.mat
+targetStart = 14;
+targetFinish = 19; 
+start = round(targetStart/sampleTime);
+finish = round(targetFinish/sampleTime);
+figure(1)
+hold on
+plot(time,states.signals.values(:,2));
+plot(time,pitchref.signals.values)
+%plot(time(start:finish),states.signals.values(start:finish,2))
+plot(simout.time,simout.signals.values)
+title('Picada')
+ylabel('\theta(rad)')
+xlabel('Tempo(s)')
+legend('Resposta de Picada experimental','Referência de Picada','Resposta de Picada H(s)','location', 'southeast')
+hold off
