48cd6ff5b6462b3aa244c3f7d41d0cc6e06f27df
Pedro <Pedrosilva704@hotmail.com>
2021-12-02 22:24:03 +0000

    Update Questão 3 e Reorganização de ficheiros

--- LAB2/parse31.m
+++ LAB2/parse31.m
@@ -1,3 +1,5 @@
+%Editar o ficheiro mat a carregar.
+
 load sensorsA.mat
 dT = sensors.signal1.time(2)-sensors.signal1.time(1);
 endTime = 30; %seconds
@@ -5,13 +7,19 @@ startTime = 0.1; %seconds
 startPlot = round(startTime/dT);
 endPlot = round(endTime/dT);
 
-treatedTimeSeries = [sensors.signal1.time(startPlot:endPlot) sensors.signal1.data(startPlot:endPlot,:)  ]; 
-
+treatedTimeSeries = [sensors.signal1.time(startPlot:endPlot) sensors.signal1.data(startPlot:endPlot,:) sensors.signal2.data(startPlot:endPlot,:)  ]; 
 %Plot block
 figure(31)
-plot( treatedTimeSeries(:,1),treatedTimeSeries(:,2:4) );
+plot( treatedTimeSeries(:,1), treatedTimeSeries(: , 2:7) );
 %enter plot details later
 
 %%Mean of time series
 
-MeanData1 = mean(treatedTimeSeries(:,2:4));
+MeanData1 = mean( treatedTimeSeries(:,2:7) );
+CovarianceData1 = cov( treatedTimeSeries(:,2:7) );
+disp(CovarianceData1);
+
+%COV([A B C]) = [COV(A,A) COV(A,B) COV(A,C)]
+%               [COV(B,A) COV(B,B) COV(B,C)]
+%               [COV(C,A) COV(C,B) COV(C,C)]
+% e COV(X,X) = VAR(X) 
