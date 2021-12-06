3b775913b7f8b3eecf2a87891f2f9e654bcc9762
arthurolago <arthurlago27@gmail.com>
2021-12-06 22:54:26 +0000

    commit

--- LAB2/parse31.m
+++ LAB2/parse31.m
@@ -1,6 +1,6 @@
 %Editar o ficheiro mat a carregar.
 
-load sensorsA.mat
+load sensorsD.mat
 dT = sensors.signal1.time(2)-sensors.signal1.time(1);
 endTime = 30; %seconds
 startTime = 0.1; %seconds
@@ -11,6 +11,7 @@ treatedTimeSeries = [sensors.signal1.time(startPlot:endPlot) sensors.signal1.dat
 %Plot block
 figure(31)
 plot( treatedTimeSeries(:,1), treatedTimeSeries(: , 2:7) );
+grid on
 %enter plot details later
 
 %%Mean of time series
@@ -19,7 +20,18 @@ MeanData1 = mean( treatedTimeSeries(:,2:7) );
 CovarianceData1 = cov( treatedTimeSeries(:,2:7) );
 disp(CovarianceData1);
 
+
+
+
 %COV([A B C]) = [COV(A,A) COV(A,B) COV(A,C)]
 %               [COV(B,A) COV(B,B) COV(B,C)]
 %               [COV(C,A) COV(C,B) COV(C,C)]
 % e COV(X,X) = VAR(X) 
+
+%accels
+g = 981;
+theta  =  (asin((treatedTimeSeries(:, 2))/g));
+phi = (asin((treatedTimeSeries(:, 3))./(g*cos(theta))));
+figure(2)
+plot (treatedTimeSeries(:, 1), rad2deg(theta), treatedTimeSeries(:, 1), rad2deg(phi))
+
