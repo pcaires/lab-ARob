c1f18b822e5fcec49991667b8098fca5930755c4
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2021-12-09 21:35:19 +0000

    updates
    Kalman

--- LAB2/parse31.m
+++ LAB2/parse31.m
@@ -1,5 +1,6 @@
 %Editar o ficheiro mat a carregar.
-load sensorsB.mat
+clear
+load sensorsD.mat
 %-------------------------------------------------
 dT = sensors.signal1.time(2)-sensors.signal1.time(1);
 endTime = 25; %seconds
@@ -8,10 +9,11 @@ startPlot = round(startTime/dT);
 endPlot = round(endTime/dT);
 
 treatedTimeSeries = [sensors.signal1.time(startPlot:endPlot) sensors.signal1.data(startPlot:endPlot,:) sensors.signal2.data(startPlot:endPlot,:)  ]; 
+treatedTimeSeries(:,2:4) = treatedTimeSeries(:,2:4)/100; % as acelerações estão em cm/s^2 
 %Plot block
 figure(31)
 plot( treatedTimeSeries(:,1), treatedTimeSeries(: , 5:7) );
-legend('\alpha_x','\alpha_y','\alpha_z') 
+legend('\omega_x','\omega_y','\omega_z') 
 title('Dados dos giroscópios')
 xlabel('tempo(s)')
 ylabel('rad/s^2')
@@ -21,7 +23,7 @@ plot( treatedTimeSeries(:,1), treatedTimeSeries(: , 2:4) );
 legend('a_x','a_y','a_z')
 title('Dados dos acelerómetros')
 xlabel('tempo(s)')
-ylabel('cm/s^2')
+ylabel('m/s^2')
 %enter plot details later
 
 %Mean of time series
@@ -35,27 +37,30 @@ disp(CovarianceData1);
 % e COV(X,X) = VAR(X) 
 
 %pitch and roll from inclinometer data
-g = 981;%cm/s^2
-theta  =  (asin((treatedTimeSeries(:, 2))/g));%pitch
-phi = (asin((treatedTimeSeries(:, 3))./(-g*cos(theta))));%roll
-phi = (atan( treatedTimeSeries(:, 3)./treatedTimeSeries(:, 4)) );
+g = 9.81;%m/s^2
+thetaRaw  =  (asin((treatedTimeSeries(:, 2))/g));%pitch
+phiRaw = (asin((treatedTimeSeries(:, 3))./(-g*cos(thetaRaw))));%roll
+phiRaw = (atan( treatedTimeSeries(:, 3)./treatedTimeSeries(:, 4)) );
 %Plot block para 3.4
 figure(33)
-plot (treatedTimeSeries(:, 1), rad2deg(theta), treatedTimeSeries(:, 1), rad2deg(phi))
+plot (treatedTimeSeries(:, 1), rad2deg(thetaRaw), treatedTimeSeries(:, 1), rad2deg(phiRaw))
 legend('raw pitch (deg)','raw roll (deg)') 
 %end plot block
+%KALMAN
 A=0;
 B=1;
 C=1;
 D=0;
-Plant = ss(A,B,C,D,-1);
-Plant.InputName = 'un';
-Plant.OutputName = 'yt';
-Sum = sumblk('un = u + w');
-sys = connect(Plant,Sum,{'u','w'},'yt');
 
-Q = 0.1;
-R = 0.1;
+Ts = dT;
+sys = ss(A, B, C, D);
+
+Q = CovarianceData1(1,1);
+R = CovarianceData1(5,5);
+Q=0.008;
+R=0.0017;
 N = 0;
-
-[Kfilter,L,P] = kalman(sys,Q,R,N);
+[kalmf,L,~,Mx,Z]= kalman(sys,Q,R,N);
+GyrosEST = [treatedTimeSeries(:,1) treatedTimeSeries(:,6)*3.14/180];
+AccelIn = [treatedTimeSeries(:,1) thetaRaw];
+kalmf = kalmf(1,:);
